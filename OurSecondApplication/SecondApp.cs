5ed66b515113ccefc860fd4c27b335e1aea2268b
Ernest Kyei <eknkrumah1@umat.edu.gh>
2023-09-20 17:00:45 +0000

    Chapter 1 Completed

---
+++ OurSecondApplication/SecondApp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondApplication
+{
+	public class SecondApp
+	{
+		public static void Run()
+		{
+			//Welcome user To Application
+			Console.WriteLine("Welcome to the Greeting Application");
+			Console.WriteLine("This application was built by Mishrule");
+			Console.WriteLine("---------------------------------------");
+			Console.WriteLine();
+
+			//Ask for First Name
+			Console.WriteLine("Please enter your first name");
+			var input = Console.ReadLine();
+
+			//Greet user by name
+			Console.WriteLine($"Welcome: {input}");
+		}
+	}
+}
---
+++ RunConsole/Run.cs
@@ -0,0 +1,17 @@
+﻿using SecondApplication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunConsole
+{
+	public class Run
+	{
+		public static void Main()
+		{
+			SecondApp.Run();
+		}
+	}
+}
