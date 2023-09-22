1f175bc9496c6a92152d79652debb0df15e5294e
Ernest Kyei <eknkrumah1@umat.edu.gh>
2023-09-22 16:56:57 +0000

    wip

---
+++ Variables/StringVariables.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    public class StringVariables
+    {
+        public static void Run()
+        {
+            string firstNameForTheUserWhoIsUsingOurApplication = string.Empty;
+            string firstName = string.Empty;
+            string lastName = string.Empty;
+
+            
+
+            Console.WriteLine($"Hello {firstName} {lastName}");
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
